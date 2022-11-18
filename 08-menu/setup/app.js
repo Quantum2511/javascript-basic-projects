bd1f6eb6c42844549abd8a33459ad1937aa2554b
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2022-11-19 05:17:37 +0530

    created dynamic content & filter buttons

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,88 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 9,
+    title: "steak dinner",
+    category: "dinner",
+    price: 69.69,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
 ];
+
+const sectionCenter = document.querySelector('.section-center')
+const btnContainer = document.querySelector('.btn-container')
+
+// load items
+window.addEventListener('DOMContentLoaded', () => {
+  displayMenuItems(menu)
+  displayMenuBtns()
+})
+
+function displayMenuItems(menuItems){
+  
+  let displayMenu = menuItems.map(item => {
+    return `<article class="menu-item">
+            <img src="${item.img}" class="photo" alt="${item.title}">
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+            </article>`
+  })   
+
+  displayMenu = displayMenu.join('')
+  sectionCenter.innerHTML = displayMenu
+}
+
+// dynamically make buttons and filter content
+function displayMenuBtns(){
+
+  // creates an array of category
+  const categories = menu.reduce(
+    function(values, item){
+      if(!values.includes(item.category)){
+        values.push(item.category)
+      }
+      return values
+  },
+  ['all'])
+  
+  makeBtn(categories)
+
+  //dynamic filter buttons based on category
+  function makeBtn(cat){
+    const btn = cat.map(category => {
+      return `<button class="filter-btn" type="button" data-id="${category}">${category}</button>`
+    }).join('')
+    btnContainer.innerHTML=btn
+  }
+  
+  const filterBtns = document.querySelectorAll('.filter-btn')
+
+  // filter items 
+  filterBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      // const category = (e.currentTarget.textContent) you can use even this method if the btn content === category to filter out like this case
+      const category = (e.currentTarget.dataset.id)
+      const menuCategory = menu.filter(menuItem => {
+        if(menuItem.category===category){
+          console.log(menuItem.category);
+          return menuItem
+        }else if(category==='all'){
+          console.log(menuItem)
+          return menuItem
+        }
+      })
+      displayMenuItems(menuCategory)
+    })
+  })
+
+}
+
