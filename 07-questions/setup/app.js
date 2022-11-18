5e08891cb6aec00ca5b70fdc528daeb4b9946acd
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2022-11-18 16:21:36 +0530

    completed app.js- using selectors inside element

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,36 @@
 //using selectors inside the element
+
+const questions =  document.querySelectorAll('.question')
+
+questions.forEach(question => {
+    const questionBtn = question.querySelector('.question-btn')
+
+    questionBtn.addEventListener('click', () => {
+        
+        questions.forEach(item => {
+            if(item!==question){
+                item.classList.remove('show-text')
+                console.log(item)
+            }
+        })
+        
+        question.classList.toggle('show-text')
+    })
+
+})
+
+
 // traversing the dom
+
+// const questionBtns = document.querySelectorAll('.question-btn')
+
+// questionBtns.forEach(btn => {
+//     btn.addEventListener('click', (e) => {
+//         const parent = (e.currentTarget.parentElement.parentElement);
+//         const allBtns = btn.parentElement.parentElement
+//         parent.classList.toggle('show-text')
+        
+//     })
+// })
+
+
