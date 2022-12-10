95f5ea75ffa5422e90792e4e0720320a3230faf9
Bhavya Chanana <65809399+Quantum2511@users.noreply.github.com>
2022-12-10 13:35:52 +0530

    learnt getBoundingClientRect method

--- 10-scroll/setup/app.js
+++ 10-scroll/setup/app.js
@@ -4,10 +4,88 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date')
+date.innerHTML = new Date().getFullYear()
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle')
+const linksContainer = document.querySelector('.links-container')
+const links = document.querySelector('.links')
 
+navToggle.addEventListener('click', () => {
+    // linksContainer.classList.toggle('show-links')
+    const containerHeight = linksContainer.getBoundingClientRect().height
+    const linksHeight = links.getBoundingClientRect().height
+    
+    if (containerHeight === 0) {        
+        linksContainer.style.height = `${linksHeight}px`
+    } else {
+        linksContainer.style.height = 0
+    }
+})
+
+const navbar = document.getElementById('nav')
+const topLink = document.querySelector('.top-link')
 // ********** fixed navbar ************
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset
+    const navbarHeight = navbar.getBoundingClientRect().height
+    
+    // closes the navbar in halfwindow mode 
+    linksContainer.style.height = 0
+
+    if (scrollHeight>navbarHeight) {
+        navbar.classList.add('fixed-nav')
+    } else {
+        navbar.classList.remove('fixed-nav')
+    }
+
+
+    // when top-link button is visible  
+
+    if (navbar.classList.contains('fixed-nav')) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    }
+  
+})
 
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll('.scroll-link')
+
+scrollLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        // prevents default bahavior
+        e.preventDefault()
+
+        // navigate to specific spot
+        const id = e.currentTarget.getAttribute('href').slice(1)
+        const element = document.getElementById(id)
+
+        // calculate the heights
+        const navbarHeight = navbar.getBoundingClientRect().height
+        const containerHeight = linksContainer.getBoundingClientRect().height
+        const fixedNav = navbar.classList.contains('fixed-nav')
+        let position = element.offsetTop - navbarHeight
+        
+        if(!fixedNav) {
+            position = position - navbarHeight
+        }
+        if(navbarHeight > 82) {
+            position = position + containerHeight
+        }
+
+        console.log(containerHeight)
+        console.log('= ', navbarHeight)
+        console.log(position)
+
+
+        window.scrollTo({
+            left:0,
+            top:position
+        })
+
+    })
+})
